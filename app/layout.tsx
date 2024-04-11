c0d1b93ed7d31e374fe3edfb277b38cc7d4b575c
Sebastián G <seba.gallardog@duocuc.cl>
2024-04-11 15:25:05 -0400

    fix default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
+                    defaultTheme="system"
                     enableSystem
                     disableTransitionOnChange
                 >

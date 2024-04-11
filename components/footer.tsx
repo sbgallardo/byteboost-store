47b260f93794742a4a8b83442120a4e5513db2ba
Sebastián G <seba.gallardog@duocuc.cl>
2024-04-10 22:02:33 -0400

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,28 @@
+import Link from "next/link";
+
 export default function Footer() {
+
+    const name :string = "ByteBoost";
+
     return (
         <>
-            <footer className="bg-gray-800">
+            <footer className="border-t">
                 <div className="mx-auto w-full max-w-screen-xl">
                     <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Empresa</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <a href="#" className=" hover:underline">Nosotros</a>
+                                    <Link href="/about" className=" hover:underline">Nosotros</Link>
                                 </li>
                                 <li className="mb-4">
-                                    <a href="#" className="hover:underline">Trabajadores</a>
+                                    <a href="/about#team" className="hover:underline">Trabajadores</a>
                                 </li>
                                 <li className="mb-4">
-                                    <a href="#" className="hover:underline">Tiendas</a>
+                                    <a href="/about#stores" className="hover:underline">Tiendas</a>
                                 </li>
                                 <li className="mb-4">
-                                    <a href="#" className="hover:underline">Equipo</a>
+                                    <a href="/about#brands" className="hover:underline">Marcas</a>
                                 </li>
                             </ul>
                         </div>
@@ -73,10 +78,11 @@ export default function Footer() {
                         </div>
                     </div>
                     <div className="flex justify-center p-3">
-                <span className="text-gray-500 mb-3">© 2024 <a
-                    href="https://www.youtube.com/watch?v=xvFZjo5PgG0">TengoTEC</a>.
-                    Todos los derechos reservados
-                </span>
+                        <span className="text-gray-500 mb-3 ">
+                            <span>© 2024 </span>
+                            <Link href="/" className="hover:underline">{name}</Link>.
+                            Todos los derechos reservados
+                        </span>
                     </div>
                 </div>
             </footer>

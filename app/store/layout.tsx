4a1e46a6333b0f6d6b325e496a4d5c6d6585c9da
Sebastián G <seba.gallardog@duocuc.cl>
2024-04-19 20:42:56 -0400

    page layout

---
+++ app/store/layout.tsx
@@ -0,0 +1,24 @@
+import { Header } from "@/components/Header";
+import Footer from "@/components/footer";
+import Collections from "@/components/store/collections";
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Header/>
+            <div
+                className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white mt-2">
+                <div className="order-first w-full flex-none md:max-w-[125px]">
+                    <Collections/>
+                </div>
+                <div className="order-last min-h-screen w-full md:order-none">{children}</div>
+                <div className="order-none flex-none md:order-last md:w-[125px]">
+                    <div>filters</div>
+                    {/*<FilterList list={sorting} title="Sort by"/>*/}
+                </div>
+            </div>
+            <Footer/>
+        </>
+    )
+}

58c6470df07035822914762235168c5fc163a515
Sebastián G <seba.gallardog@duocuc.cl>
2024-04-19 20:45:17 -0400

    Store filters

---
+++ components/store/filters.tsx
@@ -0,0 +1,58 @@
+import { Suspense } from 'react';
+import { Category } from "@/lib/types";
+import Link from 'next/link'
+
+function FilterItemList({ list }: { list: Category[] | [] }) {
+    return (
+        <>
+            {list.map((item: Category, i) => (
+                <FilterItem key={i} item={item} />
+            ))}
+        </>
+    );
+}
+
+function FilterItem({item}: {item:Category}) {
+    return (
+        <>
+            <Link href={`?category=${item.name}`}>
+                <span>
+                {item.name}
+                </span>
+            </Link>
+            <br/>
+        </>
+    )
+}
+
+export default function FilterList({ list, title }: { list: Category[] | []; title?: string }) {
+    return (
+        <>
+            <nav>
+                {title ? (
+                    <h3 className="hidden text-xs text-neutral-500 md:block dark:text-neutral-400">
+                        {title}
+                    </h3>
+                ) : null}
+                <ul className="hidden md:block">
+                    <Suspense fallback={null}>
+                        <FilterItemList list={list} />
+                    </Suspense>
+                </ul>
+                <ul className="md:hidden">
+                    <Suspense fallback={null}>
+                        <FilterItemDropdown list={list} />
+                    </Suspense>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+function FilterItemDropdown({list}: {list: Category[] | []}) {
+    return (
+        <>
+
+        </>
+    )
+}

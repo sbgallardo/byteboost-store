eb1b8ea28e65a6f5357533e7d0f8c9493fbae4b7
Sebastián G <seba.gallardog@duocuc.cl>
2024-04-19 20:45:47 -0400

    Profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,9 @@
-import {Header} from "@/components/Header";
-import Footer from "@/components/footer";
-
 
 export default function Page() {
 
-
     return (
-        <main>
-            <Header/>
+        <>
 
-
-            <Footer/>
-        </main>
+        </>
     )
 }

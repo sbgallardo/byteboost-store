a1dc0da776f8e1679d1346abfb059690f8c8fd4e
EliasFRIKI23 <124476945+EliasFRIKI23@users.noreply.github.com>
2024-05-11 17:48:51 -0400

    Tablas
    Agrege el componente de las tablas en una version temprana, la parte visual esta lista, falta conexion con BD

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Footer from "@/components/footer";
 import { Header } from "@/components/Header"
 import Homepage from "@/components/homepage";
+import {TableDemo} from "@/components/table"
 
 export default function Home() {
   return (
       <>
           <Header/>
-
+          <TableDemo />
 
           <Footer />
       </>
+      
   );
 }
---
+++ components/dialog.tsx
@@ -0,0 +1,10 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
+  

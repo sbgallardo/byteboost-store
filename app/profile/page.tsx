3fcd232500386b7f7479c65fc309a474447388b5
Sebastián G <sgallardo@tuta.io>
2024-06-06 23:29:43 -0400

    Improved Profile Page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,30 +3,45 @@ import {
     Card,
     CardHeader,
     CardTitle,
-    CardContent,
     CardDescription,
     CardFooter
 } from '@/components/ui/card'
 import {Button} from "@/components/ui/button";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage
+} from "@/components/ui/avatar";
+import Link from "next/link";
+import { MdExitToApp } from "react-icons/md";
 
 export default withPageAuthRequired(async function Page() {
-
     const session = await getSession();
-
     const user = session ? session.user : null;
 
     return (
         <>
             {!user ? null : (
-                <div>
+                <div className="my-10">
                     <div className="container lg:px-28 ">
-                        <Card>
-                            <CardHeader>
+                        <Card className="flex grid gap-3 items-center justify-center text-center">
+                            <CardHeader className="grid gap-2">
                                 <CardTitle>{user.nickname}</CardTitle>
+                                <div className="flex justify-center">
+                                    <Avatar className="h-16 w-16">
+                                        <AvatarImage src={user.picture}/>
+                                        <AvatarFallback>?</AvatarFallback>
+                                    </Avatar>
+                                </div>
                                 <CardDescription>{user.email}</CardDescription>
                             </CardHeader>
                             <CardFooter>
-                                <Button>Log out</Button>
+                                <Link href="/api/auth/logout">
+                                    <Button variant="ghost">
+                                        <MdExitToApp className="mr-2"/>
+                                        Cerrar sesión
+                                    </Button>
+                                </Link>
                             </CardFooter>
                         </Card>
                     </div>

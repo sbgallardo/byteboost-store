ca36b742b50576171e079ddd4c6aff202fe26140
Sebastián G <sgallardo@tuta.io>
2024-06-09 17:15:27 -0400

    Allow any url in images

--- next.config.mjs
+++ next.config.mjs
@@ -4,28 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "media.spdigital.cl",
-                port: '',
-            },
-            {
-                protocol: "https",
-                hostname: "i.blogs.es",
-                port: '',
-            },
-            {
-                protocol: "https",
-                hostname: "goldengamers.cl",
-                port: '',
-            },
-            {
-                protocol: "https",
-                hostname: "www.asus.com",
-                port: '',
-            }
-            ,
-            {
-                protocol: "https",
-                hostname: "upload.wikimedia.org",
+                hostname: "*",
                 port: '',
             }
         ]
